af2b7f19992bfc08cf8570cc8ac1bce75fd4f1b2
Yohan <yohan.dordolo@orange.fr>
2019-09-26 16:14:58 +0200

    max_value function

--- S1_algotools.py
+++ S1_algotools.py
@@ -26,20 +26,40 @@ def average_above_zero(table:list):
     ##
     #Function that return the table average above zero
     #Args:
-    #    table: a table
+    #    @param: the table
     #Returns the average
     
     som = 0
     n = 0
     for i in range(len(table)):
         if table[i] > 0:
-            print('tab[{index}={val}]'.format(index = i, val=table[i]))
+            #print('tab[{index}={val}]'.format(index = i, val=table[i]))
             som  = som + table[i]
             n =  n + 1
     moy =  som/n
     return moy
 
+def max_value(table:list):
+    ##
+    #Function that return the max value of te table and his index
+    #Args:
+    #   @param table: the table
+    #Returns max and index
+    max = 0
+    index = 0
+    for i in range(len(table)):
+        if table[i] > max:
+            max = table[i]
+            index = i
+    return max, index
+
+#test section
 tab_list=[1,2,3,-4,6,-9]
 #tab_zeros = np.zeros(12)
-#tab_from_list = np.array(tab_list)
-print('Average = {0}'.format(average_above_zero(tab_list)))
+tab_from_list = np.array(tab_list)
+print('Average above 0 = {0}'.format(average_above_zero(tab_list)))
+print('Max value = {0}'.format(max_value(tab_list)[0]))
+print('Index of max value = {0}'.format(max_value(tab_list)[1]))
+
+
+
