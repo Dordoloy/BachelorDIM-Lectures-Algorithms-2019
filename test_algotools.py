1c4637fd0cf92925a43cc735e90a43a6409f02bd
Yohan <yohan.dordolo@orange.fr>
2019-09-30 10:51:52 +0200

    UnitTest average_above_zero

--- test_algotools.py
+++ test_algotools.py
@@ -25,4 +25,20 @@ def test_divide_by_zero():
 
 def test_average_above_zero():
     assert s1.average_above_zero(s1.tab_list) == 3
+
+def test_average_above_zero_not_a_list():
+    with pytest.raises(ValueError):
+        s1.average_above_zero(3)
+        
+def test_average_above_zero_empty_list():
+    with pytest.raises(ValueError):
+        s1.average_above_zero([])
     
+def test_average_above_zero_not_a_number_list():
+    with pytest.raises(ValueError):
+        s1.average_above_zero(['3',''])
+        
+def test_average_above_zero_division():
+    tab_list=[-1,-2,-3,-6]  
+    with pytest.raises(ZeroDivisionError):
+        s1.average_above_zero(tab_list)
