c2e06df14cadfc470c533144428074a5513eb38f
Yohan <yohan.dordolo@orange.fr>
2019-09-30 09:37:56 +0200

    baby test_average_above_zero

--- test_algotools.py
+++ test_algotools.py
@@ -3,17 +3,26 @@
 """
 Created on Sun Sep 29 21:44:54 2019
 
-@author: alben
+@author: dordoloy
 """
 
 import pytest
+import S1_algotools as s1
 
 def inc_(x):
     return x+1
 
 def test_inc():
-    assert inc(3)==5
+    assert inc_(3)==4
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         1/0
+        
+"""
+   Unit Test avege above zero
+"""
+
+def test_average_above_zero():
+    assert s1.average_above_zero(s1.tab_list) == 3
+    
