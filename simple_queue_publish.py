9d1613f590a066780c6bd40690dc170dd29bde4d
Yohan <yohan.dordolo@orange.fr>
2019-10-01 08:52:20 +0200

    simple_queue_publish and simple_queue_read

---
+++ simple_queue_publish.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 08:36:56 2019
+
+@author: dordoloy
+"""
+
+import os
+import pika
+import config
+import getpass
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+def simple_queue_publish():
+    
+    amqp_url=config.amqp_url
+    
+    
+    # Parse CLODUAMQP_URL (fallback to localhost)
+    url = os.environ.get('CLOUDAMQP_URL',amqp_url)
+    params = pika.URLParameters(url)
+    params.socket_timeout = 5
+    
+    connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+    
+    channel = connection.channel()
+    
+    channel.queue_declare(queue='presentation')
+    
+    channel.basic_publish(exchange='',
+                          routing_key='presentation',
+                          body=getpass.getuser())
+                          
+    print(" [x] Sent '{0}'".format(getpass.getuser()))
+    
+    connection.close()
+
+simple_queue_publish()
---
+++ simple_queue_read.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 08:38:30 2019
+
+@author: dordoloy
+"""
+
+import os
+import pika
+import config
+
+
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+def simple_queue_read():
+    amqp_url=config.amqp_url
+    
+    
+    # Parse CLODUAMQP_URL (fallback to localhost)
+    url = os.environ.get('CLOUDAMQP_URL',amqp_url)
+    params = pika.URLParameters(url)
+    params.socket_timeout = 5
+    
+    connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+    
+    channel = connection.channel()
+    
+    channel.queue_declare(queue='presentation')
+    
+    channel.basic_consume(queue='presentation',
+                              on_message_callback=callback,                          
+                              auto_ack=True)
+        
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+simple_queue_read()
